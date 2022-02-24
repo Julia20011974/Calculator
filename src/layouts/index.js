b0f644a4211bc264e337ab8a6a9ed97c79a7b11e
julia <julia@debian.ad.vsu>
2022-02-24 11:40:25 +0300

    style(index): remove styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,8 +4,6 @@ export const PageLayout = styled.div`
   width: 100%;
   height: 100%;
 
-  max-width: 1280px;
-
-  padding: ${({ theme }) => theme.spaces[4]}px;
-  margin: 0 auto;
+  padding: ${({ theme }) => theme.spaces[5]}px;
+  display: flex;
 `
