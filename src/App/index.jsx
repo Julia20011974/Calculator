4d0f4d8b3a2a9d02b08cf60f03b6d94a4099ee5c
julia <julia@debian.ad.vsu>
2022-02-24 11:46:21 +0300

    feat(index): added Calculator component

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,18 +1,14 @@
-import React, { lazy, Suspense } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { Suspense } from 'react'
 
-import { HOME_PAGE_ROUTE } from '@/constants'
-
-const HomePage = lazy(() => import('@/pages/Home'))
+import Header from '@/components/Header'
+import Heading from '@/pages/Home'
+import Calculator from '@/components/Calculator'
 
 export default () => (
-  <Suspense>
-    <Switch>
-      <Route
-        exact
-        path={HOME_PAGE_ROUTE}
-        component={HomePage}
-      />
-    </Switch>
-  </Suspense>
+  <React.Fragment>
+    <Header />
+    <Suspense fallback={<Heading />}>
+      <Calculator />
+    </Suspense>
+  </React.Fragment>
 )
