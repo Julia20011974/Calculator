fa5881e55cca26280d18e683d132052399f5802e
julia <julia@debian.ad.vsu>
2022-02-24 11:23:48 +0300

    feat(display): added new component

---
+++ src/components/Display/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { DisplayContainer } from './style'
+
+export default () => {
+  return <DisplayContainer>12344</DisplayContainer>
+}
---
+++ src/components/Display/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const DisplayContainer = styled.div`
+  width: 100%;
+  text-align: right;
+  padding-top: ${({ theme }) => theme.spaces[4]}px;
+  padding-bottom: ${({ theme }) => theme.spaces[2]}px;
+  padding-right: ${({ theme }) => theme.spaces[5]}px;
+  font-size: ${({ theme }) => theme.fontSizes[6]}px;
+  border-bottom: 1px solid black;
+`
