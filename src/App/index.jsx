51a23f18b48d80e6f6aecc841b0425a6cc10bd02
julia <julia@debian.ad.vsu>
2022-03-07 02:27:08 +0300

    refactor(app): refactor component

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,14 +1,44 @@
-import React, { Suspense } from 'react'
+import React, {
+  Suspense,
+  lazy,
+  useState,
+  useMemo,
+} from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 import Header from '@/components/Header'
-import Heading from '@/pages/Home'
-import Calculator from '@/components/Calculator'
+import Loader from '@/components/Loader'
+import {
+  HOME_PAGE_ROUTE,
+  SETTINGS_PAGE_ROUTE,
+  themes,
+  ThemeContext,
+} from '@/constants'
 
-export default () => (
-  <React.Fragment>
-    <Header />
-    <Suspense fallback={<Heading />}>
-      <Calculator />
-    </Suspense>
-  </React.Fragment>
-)
+const HomePage = lazy(() => import('@/pages/Home'))
+const SettingsPage = lazy(() => import('@/pages/Settings'))
+
+export default () => {
+  const [theme, setTheme] = useState(themes[0])
+  const value = useMemo(() => ({ theme, setTheme }))
+
+  return (
+    <ThemeContext.Provider value={value}>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route
+            exact
+            path={HOME_PAGE_ROUTE}
+            component={HomePage}
+          />
+          <Route
+            exact
+            path={SETTINGS_PAGE_ROUTE}
+            component={SettingsPage}
+          />
+        </Switch>
+      </Suspense>
+    </ThemeContext.Provider>
+  )
+}
