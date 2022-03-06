ea8b1d5016bafcf55734515f862df5ac9a249e42
julia <julia@debian.ad.vsu>
2022-03-07 02:26:19 +0300

    feat(keypad): added calculating  logic

--- src/components/Keypad/index.jsx
+++ src/components/Keypad/index.jsx
@@ -1,40 +1,81 @@
 import React from 'react'
-import { KeypadContainer, ButtonContainer } from './style'
+import { KeypadContainer, KeyContainer } from './style'
+import Key from '@/components/Key'
+import { Calculator } from '@/utils/Calculator'
 
-export default () => {
+export default props => {
+  const { value, setValue } = props
+
+  const changeValue = event => {
+    let newValue = value
+
+    if (newValue === '0') newValue = ''
+
+    setValue(newValue + event.target.value)
+  }
+
+  const deleteChar = () => {
+    let newValue = value
+
+    if (newValue[newValue.length - 1] === ' ')
+      newValue = newValue.substr(0, newValue.length - 2)
+    else newValue = newValue.substr(0, newValue.length - 1)
+
+    if (newValue.length === 0) newValue = '0'
+
+    setValue(newValue)
+  }
+
+  const clearAll = () => {
+    setValue('0')
+  }
+
+  const calculate = () => {
+    // удаляем все пробелы и пустые строки
+    const arr = value.split(' ').filter(item => item !== '')
+    if (isNaN(arr[arr.length - 1]))
+      alert('Ошибка! Неверное выражение, попробуйте снова ')
+    else {
+      localStorage.setItem(value, value)
+      const calculator = Calculator(value)
+      calculator.calculate()
+      setValue(calculator.getResult())
+    }
+  }
+
   return (
     <KeypadContainer>
-      <ButtonContainer>
-        <button>C</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>*</button>
-      </ButtonContainer>
+      <KeyContainer>
+        <Key value="C" onClick={deleteChar} />
+        <Key value="7" onClick={changeValue} />
+        <Key value="8" onClick={changeValue} />
+        <Key value="9" onClick={changeValue} />
+        <Key value=" * " onClick={changeValue} />
+      </KeyContainer>
 
-      <ButtonContainer>
-        <button>-</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>\</button>
-      </ButtonContainer>
+      <KeyContainer>
+        <Key value=" - " onClick={changeValue} />
+        <Key value="4" onClick={changeValue} />
+        <Key value="5" onClick={changeValue} />
+        <Key value="6" onClick={changeValue} />
+        <Key value=" / " onClick={changeValue} />
+      </KeyContainer>
 
-      <ButtonContainer>
-        <button>+</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>=</button>
-      </ButtonContainer>
+      <KeyContainer>
+        <Key value=" + " onClick={changeValue} />
+        <Key value="1" onClick={changeValue} />
+        <Key value="2" onClick={changeValue} />
+        <Key value="3" onClick={changeValue} />
+        <Key value="=" onClick={calculate} />
+      </KeyContainer>
 
-      <ButtonContainer>
-        <button>.</button>
-        <button>(</button>
-        <button>0</button>
-        <button>)</button>
-        <button>CE</button>
-      </ButtonContainer>
+      <KeyContainer>
+        <Key value="." onClick={changeValue} />
+        <Key value="( " onClick={changeValue} />
+        <Key value="0" onClick={changeValue} />
+        <Key value=" )" onClick={changeValue} />
+        <Key value="CE" onClick={clearAll} />
+      </KeyContainer>
     </KeypadContainer>
   )
 }
--- src/components/Keypad/style.js
+++ src/components/Keypad/style.js
@@ -4,16 +4,9 @@ export const KeypadContainer = styled.div`
   width: 100%;
 `
 
-export const ButtonContainer = styled.div`
+export const KeyContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin: ${({ theme }) => theme.spaces[4]}px
     ${({ theme }) => theme.spaces[5]}px;
-  & > button {
-    padding: ${({ theme }) => theme.spaces[4]}px;
-    flex-basis: 100px;
-    border-radius: 15px;
-    cursor: pointer;
-    font-size: ${({ theme }) => theme.fontSizes[4]}px;
-  }
 `
