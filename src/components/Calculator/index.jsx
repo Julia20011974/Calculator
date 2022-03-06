e35eec7abde780f5e99553525bf0a220a322718d
julia <julia@debian.ad.vsu>
2022-03-07 02:23:55 +0300

    refactor(calculator): refactor component

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,29 +1,20 @@
-import React, { lazy, Suspense } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { useState } from 'react'
+import Keypad from '@/components/Keypad'
+import Display from '@/components/Display'
+import History from '@/components/History'
 
-import {
-  HOME_PAGE_ROUTE,
-  SETTINGS_PAGE_ROUTE,
-} from '@/constants'
-
-const HomePage = lazy(() => import('@/pages/Home'))
-const SettingsPage = lazy(() => import('@/pages/Settings'))
+import { Container } from './style'
 
 export default () => {
+  const [value, setValue] = useState('0')
+
   return (
     <React.Fragment>
-      <Switch>
-        <Route
-          exact
-          path={HOME_PAGE_ROUTE}
-          component={HomePage}
-        />
-        <Route
-          exact
-          path={SETTINGS_PAGE_ROUTE}
-          component={SettingsPage}
-        />
-      </Switch>
+      <Container>
+        <Display value={value} />
+        <Keypad value={value} setValue={setValue} />
+      </Container>
+      <History />
     </React.Fragment>
   )
 }
